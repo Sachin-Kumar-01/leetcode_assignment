0865bca4d40a40ecf8903135bac910dfa0776f8d
Sachin Kumar <anujchaudhary6397@gmail.com>
2026-07-30 23:30:48 +0530

    Time: 4 ms (41.5%), Space: 172.3 MB (92.93%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,38 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int arr[]=new int[nums.length];
-        int i=0;
-        int j=1;
-        for(int k=0;k<nums.length;k++){
-            if(nums[k]>0){
-                arr[i]=nums[k];
-                i=i+2;
+        // int arr[]=new int[nums.length];
+        // int i=0;
+        // int j=1;
+        // for(int k=0;k<nums.length;k++){
+        //     if(nums[k]>0){
+        //         arr[i]=nums[k];
+        //         i=i+2;
+        //     }
+        //     else{
+        //         arr[j]=nums[k];
+        //         j=j+2;
+        //     }
+        // }
+        // return arr;
+        int pos_arr[]=new int[nums.length/2];
+        int pos_ind=0;
+        int neg_arr[]=new int[nums.length/2];
+        int neg_ind=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                pos_arr[pos_ind]=nums[i];
+                pos_ind++;
             }
             else{
-                arr[j]=nums[k];
-                j=j+2;
+                neg_arr[neg_ind]=nums[i];
+                neg_ind++;
             }
         }
-        return arr;
+        for(int i=0;i<nums.length/2;i++){
+            nums[2*i]=pos_arr[i];
+            nums[2*i+1]=neg_arr[i];
+        }
+        return nums;
     }
 }
