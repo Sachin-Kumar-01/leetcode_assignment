c66a48987ca33846185f798ffb8c334c838826c2
Sachin Kumar <anujchaudhary6397@gmail.com>
2026-08-07 00:33:04 +0530

    Time: 3 ms (95.83%), Space: 51.5 MB (98.95%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -6,31 +6,31 @@ class Solution {
         int zeroes=0;
 
 
-        // while(right<nums.length){
-        //     if(nums[right]==0){
-        //         zeroes++;
-
-        //     }
-        //     while(zeroes>k){
-        //         if(nums[left]==0) zeroes--;
-        //         left++;
-        //     }
-        //     maxlen=Math.max(maxlen,right-left+1);
-        //     right++;
-        // }
-        // return maxlen;
-
         while(right<nums.length){
-            if(nums[right]==0) zeroes++;
-            if(zeroes<=k){
-            maxlen=Math.max(maxlen,right-left+1);
+            if(nums[right]==0){
+                zeroes++;
+
             }
-            else {
+            while(zeroes>k){
                 if(nums[left]==0) zeroes--;
                 left++;
             }
+            maxlen=Math.max(maxlen,right-left+1);
             right++;
         }
         return maxlen;
+
+        // while(right<nums.length){
+        //     if(nums[right]==0) zeroes++;
+        //     if(zeroes<=k){
+        //     maxlen=Math.max(maxlen,right-left+1);
+        //     }
+        //     else {
+        //         if(nums[left]==0) zeroes--;
+        //         left++;
+        //     }
+        //     right++;
+        // }
+        // return maxlen;
     }
 }
