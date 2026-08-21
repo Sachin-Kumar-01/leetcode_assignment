8d4100239fe39fe5d621c69e6a5e3d1e9a28b9ff
Sachin Kumar <anujchaudhary6397@gmail.com>
2026-08-21 22:30:30 +0530

    Time: 1 ms (98.32%), Space: 42.8 MB (8.55%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        int left=0;
+        int right=x;
+        int maxans=0;
+        while(left<=right){
+        int ans=0;
+
+            int mid=left+(right-left)/2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+            maxans=Math.max(maxans,ans);
+        }
+        return maxans;
+    }
+}
